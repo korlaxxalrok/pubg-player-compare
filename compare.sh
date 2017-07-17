1d7a876a074a4116cea6eec9324ec85509a62249
Brian Clark <clark.bg@gmail.com>
2017-07-17 00:24:35 -0400

    Initial script commit

---
+++ compare.sh
@@ -0,0 +1,14 @@
+#!/usr/bin/env bash
+
+#
+# Name: compare.sh
+#
+# Purpose: Compare and display stats for two (or more) players that are being tracked on https://pubgtracker.com.
+# The gears that turn: Bash + jq.
+#
+# Some tips:
+# - Create a local shell variable for your API key when you need to run this script.
+# - echo api-key="[APIKEY]"
+#
+
+
