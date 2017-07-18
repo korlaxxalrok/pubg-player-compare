61a5dc09c256efaa78ab9d100d9dfb2c014561da
Brian Clark <clark.bg@gmail.com>
2017-07-17 22:11:18 -0400

    Make variable names a little clearer

--- compare.sh
+++ compare.sh
@@ -1,4 +1,4 @@
-#!/usr/bin/env bash -x
+#!/usr/bin/env bash
 
 #
 # Name: compare.sh
@@ -30,8 +30,21 @@ do
   # Process with jq until we have a slimmed down JSON object to work with
   jq '.Stats[] | select(.Region == "agg")' data.json > filtered1.json
   jq '. | select(.Season == "2017-pre2")' filtered1.json > filtered2.json
-  jq '. | select(.Match == "solo")' filtered2.json > filtered3.json
+  jq '. | select(.Match == "solo")' filtered2.json > filtered_final.json
+
+  # Filter for statistics and assign to variables
+  kd_ratio=$(jq -r '.Stats[0].displayValue' filtered_final.json)
+  rating=$(jq -r '.Stats[9].displayValue' filtered_final.json)
+  win_percentage=$(jq -r '.Stats[1].displayValue' filtered_final.json)
+
+  # Output data
+  # test
+  echo "${p} ${kd_ratio} ${rating} ${win_percentage}"
+
 done < players
+# TODO - Create examples dir for config and players files (necessary?)
+# TODO - Spruce up repo
+# TODO - Call 'er' Dunn
 
 
 
